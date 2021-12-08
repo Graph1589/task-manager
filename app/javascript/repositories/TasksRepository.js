abe670790f952f7c281d2123ba17117ed4013fbd
graph1589 <graph1589@gmail.com>
2021-12-08 17:00:00 +0300

    added tasks crud on frontend"

--- app/javascript/repositories/TasksRepository.js
+++ app/javascript/repositories/TasksRepository.js
@@ -13,17 +13,17 @@ export default {
   },
 
   update(id, task = {}) {
-    const path = routes.apiV1TasksPath(id);
-    return FetchHelper.put(path, task);
+    const path = routes.apiV1TaskPath(id);
+    return FetchHelper.put(path, { task });
   },
 
   create(task = {}) {
     const path = routes.apiV1TasksPath();
-    return FetchHelper.post(path, task);
+    return FetchHelper.post(path, { task });
   },
 
   destroy(id) {
-    const path = routes.apiV1TasksPath(id);
+    const path = routes.apiV1TaskPath(id);
     return FetchHelper.delete(path);
   },
 };
