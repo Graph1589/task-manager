d1a5e28a19451ae23a491df8b555bca8ad74905c
graph1589 <graph1589@gmail.com>
2021-12-27 14:54:01 +0300

    changed ga workflow

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,31 +1,21 @@
-if ENV["COVERAGE"]
-  require "simplecov"
-  # start measuring the code coverage
-  SimpleCov.start do
-    # don't measure coverage of the tests themselves
-    add_filter "/test/"
+if ENV['COVERAGE']
+  require 'simplecov'
+  require 'coveralls'
+  Coveralls.wear!
+
+SimpleCov.start 'rails' do
+  require 'simplecov-lcov'
+
+  SimpleCov::Formatter::LcovFormatter.config do |c|
+    c.report_with_single_file = true
+    c.single_report_path = 'coverage/lcov.info'
   end
 
-  srcdir = File.expand_path("../src", __dir__)
+  formatter SimpleCov::Formatter::LcovFormatter
+    
+  add_filter %w[version.rb initializer.rb]
+  end
 
-  # track all ruby files under src
-  SimpleCov.track_files("#{srcdir}/**/*.rb")
-
-  # additionally use the LCOV format for on-line code coverage reporting at CI
-    require "simplecov-lcov"
-
-    SimpleCov::Formatter::LcovFormatter.config do |c|
-      c.report_with_single_file = true
-      # this is the default Coveralls GitHub Action location
-      # https://github.com/marketplace/actions/coveralls-github-action
-      c.single_report_path = "coverage/lcov.info"
-    end
-
-    # generate both HTML and LCOV reports
-    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-      SimpleCov::Formatter::HTMLFormatter,
-      SimpleCov::Formatter::LcovFormatter
-    ]
 end
 
 ENV['RAILS_ENV'] ||= 'test'
