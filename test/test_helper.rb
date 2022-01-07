8f7401291f86ee3dea13db96a9e03355327ed098
graph1589 <graph1589@gmail.com>
2022-01-07 14:40:39 +0300

    fixed codestyle offenses

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,17 +3,17 @@ if ENV['COVERAGE']
   require 'coveralls'
   Coveralls.wear!
 
-SimpleCov.start 'rails' do
-  require 'simplecov-lcov'
+  SimpleCov.start('rails') do
+    require 'simplecov-lcov'
 
-  SimpleCov::Formatter::LcovFormatter.config do |c|
-    c.report_with_single_file = true
-    c.single_report_path = 'coverage/lcov.info'
-  end
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
 
-  formatter SimpleCov::Formatter::LcovFormatter
-    
-  add_filter %w[version.rb initializer.rb]
+    formatter SimpleCov::Formatter::LcovFormatter
+
+    add_filter ['version.rb', 'initializer.rb']
   end
 
 end
