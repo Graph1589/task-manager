bf81c48604c1cb9681ec03ab847fdffa77fee2e2
graph1589 <graph1589@gmail.com>
2022-01-10 17:21:27 +0300

    implemented notifications on task updating/deleting

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,18 @@ class UserMailer < ApplicationMailer
 
     mail(to: user.email, subject: 'New Task Created')
   end
+
+  def task_updated
+    @task = params[:task]
+    author = @task.author
+
+    mail(to: author.email, subject: 'Task Updated')
+  end
+
+  def task_destroyed
+    @task = params[:task]
+    author = @task.author
+
+    mail(to: author.email, subject: 'Task Destroyed')
+  end
 end
